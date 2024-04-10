08b1c032374a553b55802f3d88a9b277181cec3f
StevenAwo <rotimiawomodu@gmail.com>
2024-04-10 22:41:05 +0100

    TASK 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,13 +4,13 @@ import signUpUser from './4-user-promise';
 
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const photo = await uploadPhoto(fileName).catch((error) => ({
-    status: 'rejected',
-    value: `${error.name}: ${error.message}`,
-  }));
   const user = await signUpUser(firstName, lastName).then((value) => ({
     status: 'fulfilled',
     value,
+  }));
+  const photo = await uploadPhoto(fileName).catch((error) => ({
+    status: 'rejected',
+    value: `${error.name}: ${error.message}`,
   }));
   return [user, photo];
 }
