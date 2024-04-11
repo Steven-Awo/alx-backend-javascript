86c9dd48fa6a6e9bb443e6148ace6e0ababd265c
StevenAwo <rotimiawomodu@gmail.com>
2024-04-11 19:09:14 +0100

    TASK 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -3,11 +3,11 @@ export default class HolbertonCourse {
       if (typeof name !== 'string') {
         throw TypeError('name must be a String');
       }
-      if (!Array.isArray(students)) {
-        throw TypeError('students must be an Array');
-      }
       if (typeof length !== 'number') {
         throw TypeError('length must be a Number');
+      }
+      if (!Array.isArray(students)) {
+        throw TypeError('students must be an Array');
       }
       students.forEach((student) => {
         if (typeof student !== 'string') throw TypeError('student must be a String');
@@ -20,21 +20,25 @@ export default class HolbertonCourse {
     get name() {
       return this._name;
     }
+  
     get length() {
       return this._length;
     }
+  
     get students() {
       return this._students;
     }
-
+  
     set name(newName) {
       if (typeof newName !== 'string') throw TypeError('name must be a String');
       this._name = newName;
     }
+  
     set length(newLength) {
       if (typeof newLength !== 'number') throw TypeError('length must be a Number');
       this._length = newLength;
     }
+  
     set students(newStudents) {
       if (!Array.isArray(newStudents)) throw TypeError('students must be an Array');
       newStudents.forEach((student) => {
@@ -43,3 +47,4 @@ export default class HolbertonCourse {
       this._students = newStudents;
     }
   }
+  
