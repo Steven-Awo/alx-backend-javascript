99b352423c53ca31bc23ce6818f8f50051d7ed00
StevenAwo <rotimiawomodu@gmail.com>
2024-04-11 19:30:36 +0100

    TASK 3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -6,20 +6,25 @@ export default class Currency {
     this._code = code;
     this._name = name;
   }
+
   get code() {
     return this._code;
   }
+
   get name() {
     return this._name;
   }
+
   set code(newCode) {
     if (typeof code !== 'string') throw TypeError('student must be a String');
     this._code = newCode;
   }
+
   set name(newName) {
     if (typeof newName !== 'string') throw TypeError('student must be a String');
     this._name = newName;
   }
+
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
