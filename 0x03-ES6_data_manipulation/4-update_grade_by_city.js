fbf6f584f18e6c5c46aed9e617c520e35fc601fc
StevenAwo <rotimiawomodu@gmail.com>
2024-04-16 10:20:33 +0100

    TASK 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,9 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
     return students
       .filter((student) => student.location.localeCompare(city) === 0)
-      .map((a) => {
-        const newGrade = newGrades.filter((b) => b.studentId === a.id);
-        const student = a;
+      .map((x) => {
+        const newGrade = newGrades.filter((b) => b.studentId === x.id);
+        const student = x;
         if (newGrade.length === 1) student.grade = newGrade[0].grade;
         else student.grade = 'N/A';
         return student;
