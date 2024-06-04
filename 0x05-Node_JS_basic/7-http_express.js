f777d0e370887e14b4a7ffc1978dbca590125ebe
StevenAwo <rotimiawomodu@gmail.com>
2024-06-04 20:55:00 +0100

    TASK 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,38 +1,39 @@
-const http = require('http');
+const express = require('express');
 const fs = require('fs');
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
       if (err) return reject(Error('Cannot load the database'));
-      // spliting the data and taking just only the
-      list without headder
-      const linnes = data.split('\n').slice(1, -1);
-      // gives just the headder of the data
-      const headder = data.split('\n').slice(0, 1)[0].split(',');
-      // finding the firstname and then the field index
-      const idxFirstname = headder.findIndex((ele) => ele === 'firstname');
-      const idxFdist = headder.findIndex((ele) => ele === 'field');
-      // declarate two dictionaries 
-      const fieldds = {};
-      const studdents = {};
-      // it should contain the all data
+      // split data and taking only list without header
+      const lines = data.split('\n').slice(1, -1);
+      // give the header of data
+      const header = data.split('\n').slice(0, 1)[0].split(',');
+      // find firstname and field index
+      const idxFn = header.findIndex((ele) => ele === 'firstname');
+      const idxFd = header.findIndex((ele) => ele === 'field');
+      // declarate two dictionaries for count each fields and store list of students
+      const fields = {};
+      const students = {};
+      // it will contain all data
       const all = {};
 
-      linnes.forEach((line) => {
+      lines.forEach((line) => {
         const list = line.split(',');
-        if (!fieldds[list[idxFdist]]) fieldds[list[idxFdist]] = 0;
-        fieldds[list[idxFdist]] += 1;
-        if (!studdents[list[idxFdist]]) studdents[list[idxFdist]] = '';
-        studdents[list[idxFdist]] += studdents[list[idxFdist]] ? `, ${list[idxFirstname]}` : list[idxFirstname];
+        if (!fields[list[idxFd]]) fields[list[idxFd]] = 0;
+        fields[list[idxFd]] += 1;
+        if (!students[list[idxFd]]) students[list[idxFd]] = '';
+        students[list[idxFd]] += students[list[idxFd]]
+          ? `, ${list[idxFn]}`
+          : list[idxFn];
       });
 
-      all.numberStudents = `Number of students: ${linnes.length}\n`;
+      all.numberStudents = `Number of students: ${lines.length}\n`;
       all.listStudents = [];
-      for (const keyy in fieldds) {
-        if (Object.hasOwnProperty.call(fieldds, keyy)) {
-          const elementt = fieldds[keyy];
-          all.listStudents.push(`Number of students in ${keyy}: ${elementt}. List: ${studdents[keyy]}`);
+      for (const key in fields) {
+        if (Object.hasOwnProperty.call(fields, key)) {
+          const element = fields[key];
+          all.listStudents.push(`Number of students in ${key}: ${element}. List: ${students[key]}`);
         }
       }
       return resolve(all);
@@ -41,7 +42,6 @@ function countStudents(path) {
 }
 
 const app = express();
-
 const port = 1245;
 
 app.get('/', (req, res) => {
@@ -49,18 +49,15 @@ app.get('/', (req, res) => {
 });
 app.get('/students', (req, res) => {
   res.write('This is the list of our students\n');
-
   countStudents(process.argv[2])
     .then((data) => {
       res.write(data.numberStudents);
-
       res.end(data.listStudents.join('\n'));
     })
     .catch((err) => {
       res.end(err.message);
     });
 });
-
 app.listen(port);
 
 module.exports = app;
